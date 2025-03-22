e56260bee22751a79f4d9dfe8986cd0e1080818b
Miran-Mirantee <killerm11805@hotmail.com>
2025-03-22 17:22:40 +0700

    add mock data

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+/**
+ * TODO:
+ * - create a data set for products (json)
+ * - create a basic UI for testing the function module
+ * - implement a discount module
+ */
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
---
+++ src/types/productType.ts
@@ -0,0 +1,16 @@
+// Define an enum for product categories
+export enum Category {
+  Clothing = "Clothing",
+  Accessories = "Accessories",
+  Electronics = "Electronics",
+  Footwear = "Footwear",
+  HomeAppliance = "Home Appliance",
+  Sports = "Sports",
+}
+
+// Define a Product type
+export type Product = {
+  name: string;
+  category: Category;
+  price: number;
+};
