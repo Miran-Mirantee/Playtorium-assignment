706acaf7398e6201b9e80486fed2755aab187c5c
Miran-Mirantee <killerm11805@hotmail.com>
2025-03-24 01:55:19 +0700

    render campaigns

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { products } from "./data/product";
+import { campaigns as campaignData } from "./data/campaign";
 import { SelectedProduct, Product, ProductCategory } from "./types/productType";
 import { Campaign, CampaignCategory, CampaignName } from "./types/campaignType";
 import applyDiscounts from "./modules/discount";
@@ -8,75 +9,24 @@ function App() {
   const [selectedProducts, setSelectedProducts] = useState<SelectedProduct[]>(
     []
   );
+  const [campaigns, setCampaigns] = useState<Campaign[]>(campaignData);
+  const [selectedCampaigns, setSelectedCampaigns] = useState<Campaign[]>([]);
 
-  useEffect(() => {
-    // mock
-    applyDiscounts(
-      [
-        {
-          id: 6,
-          name: "Sunglasses",
-          category: ProductCategory.Accessories,
-          price: 1500,
-          count: 1,
-        },
-        {
-          id: 9,
-          name: "Smartphone",
-          category: ProductCategory.Electronics,
-          price: 25000,
-          count: 4,
-        },
-        {
-          id: 12,
-          name: "Bluetooth Speaker",
-          category: ProductCategory.Electronics,
-          price: 3200,
-          count: 1,
-        },
-      ],
-      [
-        {
-          id: 1,
-          name: CampaignName.Fixed,
-          category: CampaignCategory.Coupon,
-          params: [10, 10],
-        },
-        {
-          id: 2,
-          name: CampaignName.Percentage,
-          category: CampaignCategory.Coupon,
-          params: [10, 10],
-        },
-        {
-          id: 3,
-          name: CampaignName.PercentageCategory,
-          category: CampaignCategory.OnTop,
-          params: [10, 10],
-        },
-        {
-          id: 4,
-          name: CampaignName.Point,
-          category: CampaignCategory.OnTop,
-          params: [10, 10],
-        },
-        {
-          id: 5,
-          name: CampaignName.Special,
-          category: CampaignCategory.Seasonal,
-          params: [10, 10],
-        },
-      ]
-    );
-  }, []);
+  // useEffect(() => {
+  //   console.log(selectedProducts);
+  // }, [selectedProducts]);
 
-  useEffect(() => {
-    console.log(selectedProducts);
-  }, [selectedProducts]);
+  // useEffect(() => {
+  //   console.log(campaigns);
+  // }, [campaigns]);
+
+  // useEffect(() => {
+  //   console.log(selectedCampaigns);
+  // }, [selectedCampaigns]);
 
   const onClickAddProduct = (product: Product) => {
     setSelectedProducts((s) => {
-      if (!s.some((selectedProduct) => selectedProduct.name == product.name)) {
+      if (!s.some((selectedProduct) => selectedProduct.id == product.id)) {
         return [...s, { ...product, count: 1 }];
       }
       return s;
@@ -99,6 +49,95 @@ function App() {
           ? { ...selectedProduct, count } // New object instead of modifying the old one
           : selectedProduct
       )
+    );
+  };
+
+  const onClickAddCampaign = (campaign: Campaign) => {
+    setSelectedCampaigns((s) => {
+      if (
+        !s.some(
+          (selectedCampaign) =>
+            selectedCampaign.category == campaign.category ||
+            selectedCampaign.id == campaign.id
+        )
+      ) {
+        return [...s, campaign];
+      }
+      return s;
+    });
+  };
+
+  const onClickRemoveCampaign = (campaignId: number) => {
+    setSelectedCampaigns((s) => {
+      return [...s].filter(
+        (selectedCampaign) => selectedCampaign.id != campaignId
+      );
+    });
+  };
+
+  const onChangeCampaignParam = (
+    campaignId: number,
+    index: number,
+    param: any
+  ) => {
+    setCampaigns((c) =>
+      c.map((campaign) =>
+        campaign.id === campaignId
+          ? {
+              ...campaign,
+              params: campaign.params.map((p, i) => (i === index ? param : p)),
+            } // New object instead of modifying the old one
+          : campaign
+      )
+    );
+  };
+
+  const renderInput = (
+    campaignId: number,
+    value: number,
+    onChange: (value: number) => void
+  ) => (
+    <input
+      className="bg-amber-50 w-16 border disabled:bg-amber-300 disabled:cursor-not-allowed ml-auto"
+      type="number"
+      disabled={selectedCampaigns.some(
+        (selectedCampaign) => selectedCampaign.id == campaignId
+      )}
+      value={!isNaN(value) ? value : ""}
+      onChange={(e) => onChange(parseFloat(e.target.value))}
+      onBlur={(e) => {
+        if (e.target.value === "") {
+          onChange(1);
+        }
+      }}
+    />
+  );
+
+  const renderAddCampaignBtn = (
+    selectedCampaigns: Campaign[],
+    campaign: Campaign
+  ) => {
+    return !selectedCampaigns.some(
+      (selectedCampaign) => selectedCampaign.id == campaign.id
+    ) ? (
+      <button
+        className="py-1 px-3 rounded-md bg-green-300 cursor-pointer disabled:bg-gray-400 disabled:cursor-not-allowed"
+        disabled={selectedCampaigns.some(
+          (selectedCampaign) =>
+            selectedCampaign.category == campaign.category &&
+            selectedCampaign.id != campaign.id
+        )}
+        onClick={() => onClickAddCampaign(campaign)}
+      >
+        Add
+      </button>
+    ) : (
+      <button
+        className="py-1 px-3 rounded-md bg-red-300 cursor-pointer"
+        onClick={() => onClickRemoveCampaign(campaign.id)}
+      >
+        Remove
+      </button>
     );
   };
 
@@ -127,7 +166,7 @@ function App() {
           ))}
         </div>
 
-        <span className="flex flex-col gap-4 w-1/3">
+        <span className="flex flex-col gap-4 w-2/5">
           {/* Render list of selected products */}
           <ul className="bg-amber-100 border-2 border-dashed h-1/3 p-4 flex flex-col gap-2 overflow-auto">
             {selectedProducts.map((selectedProduct) => (
@@ -164,7 +203,92 @@ function App() {
           </ul>
 
           {/* Render list of all campaigns */}
-          <ul className="bg-amber-100 border-2 border-dashed h-1/3 p-4 flex flex-col gap-2 overflow-auto"></ul>
+          <div className="bg-amber-100 border-2 border-dashed h-1/3 p-4 flex flex-col gap-2 overflow-auto">
+            <p className="font-bold">{CampaignCategory.OnTop}</p>
+            <ul>
+              {campaigns
+                .filter(
+                  (campaign) => campaign.category == CampaignCategory.OnTop
+                )
+                .map((campaign) => (
+                  <li key={campaign.id} className="flex gap-4 mb-2">
+                    {renderAddCampaignBtn(selectedCampaigns, campaign)}
+                    <p>{campaign.name}</p>
+                    {campaign.name === CampaignName.PercentageCategory ? (
+                      <>
+                        <select
+                          className="bg-amber-50 border ml-auto disabled:bg-amber-300 disabled:cursor-not-allowed"
+                          disabled={selectedCampaigns.some(
+                            (selectedCampaign) =>
+                              selectedCampaign.id == campaign.id
+                          )}
+                          value={campaign.params[0]}
+                          onChange={(e) => {
+                            onChangeCampaignParam(
+                              campaign.id,
+                              0,
+                              e.target.value
+                            );
+                          }}
+                        >
+                          {Object.values(ProductCategory).map((category) => (
+                            <option key={category} value={category}>
+                              {category}
+                            </option>
+                          ))}
+                        </select>
+                        {renderInput(campaign.id, campaign.params[1], (value) =>
+                          onChangeCampaignParam(campaign.id, 1, value)
+                        )}
+                      </>
+                    ) : (
+                      <>
+                        {renderInput(campaign.id, campaign.params[0], (value) =>
+                          onChangeCampaignParam(campaign.id, 0, value)
+                        )}
+                      </>
+                    )}
+                  </li>
+                ))}
+            </ul>
+            <p className="font-bold">{CampaignCategory.Seasonal}</p>
+            <ul>
+              {campaigns
+                .filter(
+                  (campaign) => campaign.category == CampaignCategory.Seasonal
+                )
+                .map((campaign) => (
+                  <li key={campaign.id} className="flex gap-4 mb-2">
+                    {renderAddCampaignBtn(selectedCampaigns, campaign)}
+                    <p>{campaign.name}</p>
+                    <p className="ml-auto">At every</p>
+                    {renderInput(campaign.id, campaign.params[0], (value) =>
+                      onChangeCampaignParam(campaign.id, 0, value)
+                    )}
+                    <p>subtract by</p>
+                    {renderInput(campaign.id, campaign.params[1], (value) =>
+                      onChangeCampaignParam(campaign.id, 1, value)
+                    )}
+                  </li>
+                ))}
+            </ul>
+            <p className="font-bold">{CampaignCategory.Coupon}</p>
+            <ul>
+              {campaigns
+                .filter(
+                  (campaign) => campaign.category == CampaignCategory.Coupon
+                )
+                .map((campaign) => (
+                  <li key={campaign.id} className="flex gap-4 mb-2">
+                    {renderAddCampaignBtn(selectedCampaigns, campaign)}
+                    <p>{campaign.name}</p>
+                    {renderInput(campaign.id, campaign.params[0], (value) =>
+                      onChangeCampaignParam(campaign.id, 0, value)
+                    )}
+                  </li>
+                ))}
+            </ul>
+          </div>
 
           {/* Render discount calculation */}
           <ul className="bg-amber-100 border-2 border-dashed h-1/3 p-4 flex flex-col gap-2 overflow-auto"></ul>
